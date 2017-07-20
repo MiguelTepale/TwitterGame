d61a06dd501d2c2007ae0beb8ed9d72a7b770ffb
Miguel Tepale <miguel.tepale@gmail.com>
2017-07-20 18:33:06 -0400

    Created Sentiment Analysis API call

--- TwitterGame/StartViewController.swift
+++ TwitterGame/StartViewController.swift
@@ -17,6 +17,45 @@ class StartViewController: UIViewController {
         super.viewDidLoad()
         
         mysharedManager.downloadTweets()
+        initiateSentimentAnalysisAPI()
+    }
+    
+    func initiateSentimentAnalysisAPI() {
         
+        let urlString = "https://twinword-sentiment-analysis.p.mashape.com/analyze/?text=great+value+in+its+price+range!"
+        let url = URL(string: urlString)
+        
+        var request = URLRequest(url: url!)
+        request.setValue("hGaVDVWgzMmsh6XN8RaifWxSAjKop1vUECRjsnXRPzlJAyJiWS", forHTTPHeaderField: "X-Mashape-Key")
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let urlData = data, error == nil else {
+                return
+            }
+            
+            guard let json = try? JSONSerialization.jsonObject(with: urlData) as? [String: AnyObject] else {
+                return
+            }
+            //Sentiment = [postive,negative,neutral]
+            guard let sentiment = json?["type"] as! String? else{
+                print("'Sentiment' value is nil")
+                return
+            }
+            guard let keywords = json?["keywords"] as! [[String:Any]]? else {
+                print("'Keywords' value is nil")
+                return
+            }
+            
+            for values in keywords {
+                //keyword = e.g.[great,price,value,range]
+                guard let keyword = values["word"] as! String? else {
+                    return
+                }
+            }
+            
+            
+            }
+            .resume()
     }
 }
