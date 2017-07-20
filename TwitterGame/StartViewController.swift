9884b2b4b165279b55ce02af6121a7eaf695a306
Miguel Tepale <miguel.tepale@gmail.com>
2017-07-20 17:29:19 -0400

    Merge for a pull

--- TwitterGame/StartViewController.swift
+++ TwitterGame/StartViewController.swift
@@ -10,36 +10,19 @@ import UIKit
 import TwitterKit
 
 class StartViewController: UIViewController {
-
-//    let client = TWTRAPIClient()
+    
+        let client = TWTRAPIClient()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         
-        let client = TWTRAPIClient()
-        let tweetIDs = ["20", "510908133917487104"]
-        client.loadTweets(withIDs: tweetIDs) { (tweets, error) -> Void in
-            // handle the response or error
-            //print(tweets)
-        }
+//         Do any additional setup after loading the view.
+        
+                let client = TWTRAPIClient()
+                let tweetIDs = ["20", "510908133917487104"]
+                client.loadTweets(withIDs: tweetIDs) { (tweets, error) -> Void in
+                    // handle the response or error
+                    //print(tweets)
+                }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
