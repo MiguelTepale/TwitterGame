a73084f95699966eeb5c14998e2576c7ce04106e
Miguel Tepale <miguel.tepale@gmail.com>
2017-07-20 23:08:07 -0400

    Added UI to ResultsVC

--- TwitterGame/ResultsViewController.swift
+++ TwitterGame/ResultsViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ResultsViewController: UIViewController {
 
+    @IBOutlet weak var keepScoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
